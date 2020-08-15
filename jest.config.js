bd8318a239469935ce54212f5d94b2e2f49b611f
dyyyl <hey@dyyyl.rocks>
2020-08-15 17:44:28 +0200

    initial setup commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'json'],
+};
