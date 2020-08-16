89a4922a84bf3d12f5d56badfd9661e025708d30
LashaunnaS <shaunna191@icloud.com>
2020-08-16 16:05:07 +0200

    Added back side content and specified front side content on each card.

--- src/shared/mocks/deck.ts
+++ src/shared/mocks/deck.ts
@@ -1,50 +1,63 @@
 export default [
   {
     id: 1,
-    content: 1,
+    frontContent: 1,
+    backContent: `✨`, 
   },
   {
     id: 2,
-    content: 2,
+    frontContent: 2,
+    backContent: `🦄`, 
   },
   {
     id: 3,
-    content: 3,
+    frontContent: 3,
+    backContent: `💩`, 
   },
   {
     id: 4,
-    content: 4,
+    frontContent: 4,
+    backContent: `🦊`,
   },
   {
     id: 5,
-    content: 5,
+    frontContent: 5,
+    backContent: `🦖`, 
   },
   {
     id: 6,
-    content: 6,
+    frontContent: 6,
+    backContent: `🧠`,
   },
+  //
   {
     id: 7,
-    content: 7,
+    frontContent: 7,
+    backContent: `🦖`,
   },
   {
     id: 8,
-    content: 8,
+    frontContent: 8,
+    backContent: `🦊`,
   },
   {
     id: 9,
-    content: 9,
+    frontContent: 9,
+    backContent: `🧠`,
   },
   {
     id: 10,
-    content: 10,
+    frontContent: 10,
+    backContent: `🐙`,
   },
   {
     id: 11,
-    content: 11,
+    frontContent: 11,
+    backContent: `✨`,
   },
   {
     id: 12,
-    content: 12,
+    frontContent: 12,
+    backContent: `🦄`,
   },
 ];
