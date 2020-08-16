9d23a38ceabb47aca541edb43d43cbc5ab81f3a3
LashaunnaS <shaunna191@icloud.com>
2020-08-16 12:32:24 +0200

    Added a method to change property of isFlipped when a card is clicked and return new state.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from 'shared/components/Card';
 import Grid from 'shared/components/Grid';
@@ -6,14 +6,76 @@ import Main from 'shared/components/Main';
 import Title from 'shared/components/Title';
 
 const App = (): JSX.Element => {
-  const cardContent: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [cardContent, setCardContent] = useState([
+    {
+      id: 1,
+      content: 1,
+      isFlipped: false,
+    },
+    {
+      id: 2,
+      content: 2,
+      isFlipped: false,
+    },
+    {
+      id: 3,
+      content: 3,
+      isFlipped: false,
+    },
+    {
+      id: 4,
+      content: 4,
+      isFlipped: false,
+    },
+    {
+      id: 5,
+      content: 5,
+      isFlipped: false,
+    },
+    {
+      id: 6,
+      content: 6,
+      isFlipped: false,
+    },
+    {
+      id: 7,
+      content: 7,
+      isFlipped: false,
+    },
+    {
+      id: 8,
+      content: 8,
+      isFlipped: false,
+    },
+    {
+      id: 9,
+      content: 9,
+      isFlipped: false,
+    },
+  ]);
 
-  return (
+const updateCardFlipState = (i: number): void => {
+  // replace isFlipped value for card[i]
+  const card = cardContent[i];
+  // set inverse on isFlipped for card [i]
+  card.isFlipped = !card.isFlipped;
+
+  // create new array and replace with card
+  const newCardContent = cardContent.slice(0);
+  newCardContent[i] = card;
+
+  // set ☝🏾 to state
+  setCardContent(newCardContent);
+}
+  
+return (
     <Main>
       <Title>MEMORY TENNIS LET'S GO!</Title>
       <Grid>
-        {cardContent.map((card: number, i: number) => (
-          <Card key={i}>{card}</Card>
+        {cardContent.map((card, i) => (
+          <Card key={card.id} onClick={() => updateCardFlipState(i)} showCardBack={card.isFlipped}>
+            {card.content}
+          </Card>
         ))}
       </Grid>
     </Main>
