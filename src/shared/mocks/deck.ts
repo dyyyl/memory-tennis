87c128c59eb252043f1edcb624391713f6f5605d
dyyyl <hey@dyyyl.rocks>
2020-08-16 15:18:05 +0200

    chore(mock) mock out 'deck' data

---
+++ src/shared/mocks/deck.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    content: 1,
+  },
+  {
+    id: 2,
+    content: 2,
+  },
+  {
+    id: 3,
+    content: 3,
+  },
+  {
+    id: 4,
+    content: 4,
+  },
+  {
+    id: 5,
+    content: 5,
+  },
+  {
+    id: 6,
+    content: 6,
+  },
+  {
+    id: 7,
+    content: 7,
+  },
+  {
+    id: 8,
+    content: 8,
+  },
+  {
+    id: 9,
+    content: 9,
+  },
+];
